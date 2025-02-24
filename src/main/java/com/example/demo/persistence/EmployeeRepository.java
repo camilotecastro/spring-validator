4e5eb6b17df61721fec8c7b1530edd45e4988df2
Camilo Castro <camilo.castrotech@gmail.com>
2025-02-24 18:55:30 -0500

    Se agrega anotación repository

--- src/main/java/com/example/demo/persistence/EmployeeRepository.java
+++ src/main/java/com/example/demo/persistence/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.persistence;
 
 import com.example.demo.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
